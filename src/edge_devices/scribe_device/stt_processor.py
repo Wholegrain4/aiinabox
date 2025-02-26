16a54cd1f8c779796bd2345ef91d454668e0b532
Wholegrain4 <grain95@gmail.com>
2025-02-25 20:08:42 -0500

    another change

--- src/edge_devices/scribe_device/stt_processor.py
+++ src/edge_devices/scribe_device/stt_processor.py
@@ -1,8 +1,6 @@
 # stt_processor.py
 from utils import STTProcessor
 from gpiozero import Device
-from gpiozero.pins.lgpio import LGPIOFactory
-Device.pin_factory = LGPIOFactory()
 
 def main():
     processor = STTProcessor()
